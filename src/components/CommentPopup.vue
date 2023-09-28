8aa99892297b683ad50037a6fe896a1b9d4ed2ad
vinith patel <viniththammala12@gmail.com>
2023-09-28 12:12:29 +0530

    comments of each project and day view added

--- src/components/CommentPopup.vue
+++ src/components/CommentPopup.vue
@@ -1,33 +1,64 @@
 <template>
-  <v-dialog width="500">
-        <template v-slot:activator="{ props }">
-            <v-btn v-bind="props" variant="text" icon="mdi-comment" size="small"></v-btn>
-        </template>
-
-        <template v-slot:default="{ isActive }">
+    <div>
+        <v-card 
+            variant="text"
+        >
+        <div class="d-flex flex-row align-center justify-center">
+            <slot></slot>
+            <v-btn v-if="isCommentValid" variant="text" icon="mdi-comment" size="small"
+                @click="onClickIcon"
+            ></v-btn>
+        </div>           
+        </v-card>
+        <v-dialog width="500" v-model="dialog">
             <v-card title="Comment">
-            <v-card-text>
-                {{ comment }}
-            </v-card-text>
+                <v-card-text>
+                    {{ comment }}
+                </v-card-text>
 
-            <v-card-actions>
-                <v-spacer></v-spacer>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
 
-                <v-btn
-                text="Close"
-                @click="isActive.value = false"
-                ></v-btn>
-            </v-card-actions>
+                    <v-btn
+                    text="Close"
+                    @click="onClose"
+                    ></v-btn>
+                </v-card-actions>
             </v-card>
-        </template>
     </v-dialog>
+    </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return ({
+            dialog:false,
+        })
+    },
+
     props:[
         'comment'
     ],
+    computed:{
+        isCommentValid(){
+            if(this.comment === 'null' || this.comment === '' || this.comment === null){
+                return false ;
+            }
+            
+            return true ;
+        }
+    },
+
+    methods:{
+        onClickIcon(){
+            this.dialog = true ;
+        },
+
+        onClose(){
+            this.dialog = false ;
+        }
+    }
 
 }
 </script>
